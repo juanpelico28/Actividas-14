66b35804b4ab4b3cd52728e27ab81bcfcb60e4a3
Juan Pelicò <ref.juanlopez@gmail.com>
2026-03-17 23:55:28 -0600

    Avance de ejercicio 6

---
+++ Ejercicio 6/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Dictionary<int, Estudiante> estudiantes = new Dictionary<int, Estudiante>();
+Console.Write("¿Cuantos Estudiantes Desea Ingresar?: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{   
+    Console.WriteLine($"\n------- Ingresar Datos Del Estudiante {i + 1}: ");
+    Console.Write("Carnet: "); int carnet = int.Parse(Console.ReadLine());
+    Estudiante estu = new Estudiante();
+    Console.Write("Nombre: "); estu.Nombre = Console.ReadLine();
+    Console.Write("Carrera: "); estu.Carrera = Console.ReadLine();
+    Console.Write("Nota Final: "); estu.NotaFinal = double.Parse(Console.ReadLine());
+    estudiantes[carnet] = estu;
+}
+Console.WriteLine("\n------------ Listado General ---------------");
+foreach (KeyValuePair<int, Estudiante> item in estudiantes)
+{
+    Console.Write("Carnet: " + item.Key);
+    item.Value.MostrarDatos();
+
+}
+
+class Estudiante
+{
+    public string Nombre;
+    public string Carrera;
+    public double NotaFinal;
+
+    public string Estado() { if (NotaFinal > 61) return "Aprobado"; else return "Reprobado"; }
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"| Nombre: {Nombre} | Carrera: {Carrera} | Nota Fina: {NotaFinal} | Estado: {Estado}");
+    }
+}
